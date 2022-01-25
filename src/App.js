f0618d07b81c0da6b0f2dbd60a4e60e0d623e278
Vens <v.kolev@alfatrex.com>
2022-01-25 15:51:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [rate, setRate] = useState("");
+  const [zehler, setZehler] = useState(0);
+  const [randomZahl, setRandomZahl] = useState(
+    Math.floor(Math.random() * 10) + 1
+  );
+  const [mesage, setMesage] = useState("Bitte geben Sie ein Zahl");
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Rate ein Zahl zwichen 1 und 10</h1>
+        <input
+          value={rate}
+          onChange={(event) => {
+            setRate(event.target.value);
+          }}
+        ></input>
+        <button onClick={() => {
+          setZehler(zehler+1)
+          if(parseInt(rate) === randomZahl){
+            setMesage("Das ist richtig!");
+          } else {
+            setMesage("Das ist falsch!")
+          }
+        }}>Raten</button>
+        <p>{mesage}</p>
+        <p>Versuche: {zehler}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
